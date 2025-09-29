037ba616bebd5dc1f10c3f4259a6cf49d8c88711
Archna Pandey <archana333.cse@gmail.com>
2025-09-29 17:22:40 +0530

    update code

--- 01vitebasic/src/main.jsx
+++ 01vitebasic/src/main.jsx
@@ -3,8 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+function MyAPP(){
+  return(
+     <div>
+    <h2>Custom App!</h2>
+    </div>
+  )
+   
+  
+}
+
+const element=(
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ /*  <StrictMode>
+    <MyAPP />
+    <App/>
+  </StrictMode>, */
+  element
 )
